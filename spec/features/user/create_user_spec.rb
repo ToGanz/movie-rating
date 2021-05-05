29f570d2fd0698454159c1b48a51f0c067ad56dd
tganz <tganz5890@googlemail.com>
2021-05-05 15:50:05 +0200

    add sign up features specs

--- spec/features/user/create_user_spec.rb
+++ spec/features/user/create_user_spec.rb
@@ -1,54 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe 'create user', type: :feature do
+  let(:user) { FactoryBot.build(:user) }
+
   scenario 'valid inputs' do
-    visit signup_path
+    visit root_path
+    find('nav a', text: 'Signup').click
 
-    fill_in 'Name', with: 'Tobi'
-    fill_in 'Email', with: 'test@test.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
+    find('.sign-up-button').click
 
-    click_on 'Sign up'
-
+    expect(page).to have_selector('#user-settings')
     expect(page).to have_content('You have signed up successfully.')
   end
 
   scenario 'invalid email' do
     visit signup_path
     
-    fill_in 'Name', with: 'Tobi'
-    fill_in 'Email', with: 'testtest.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
-
-    click_on 'Sign up'
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: "test.com"
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
+    find('.sign-up-button').click
+    
+    expect(page).not_to have_selector('#user-settings')
     expect(page).to have_content('is invalid')
   end
 
   scenario 'missing name' do
     visit signup_path
     
-    fill_in 'Name', with: ''
-    fill_in 'Email', with: 'test@test.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'password'
+    fill_in 'user[name]', with: ''
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: user.password_confirmation
+    find('.sign-up-button').click
 
-    click_on 'Sign up'
-
+    expect(page).not_to have_selector('#user-settings')
     expect(page).to have_content("can't be blank")
   end
 
   scenario 'wrong password' do
     visit signup_path
     
-    fill_in 'Name', with: 'tobi'
-    fill_in 'Email', with: 'test@test.com'
-    fill_in 'Password', with: 'password'
-    fill_in 'Password confirmation', with: 'pas'
+    fill_in 'user[name]', with: user.name
+    fill_in 'user[email]', with: user.email
+    fill_in 'user[password]', with: user.password
+    fill_in 'user[password_confirmation]', with: 'pas'
+    find('.sign-up-button').click
 
-    click_on 'Sign up'
-
+    expect(page).not_to have_selector('#user-settings')
     expect(page).to have_content("doesn't match Password")
   end
 end
