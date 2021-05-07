4a1d5644256ff3999ce952611f546dff8df1fb4e
tganz <tganz5890@googlemail.com>
2021-05-07 10:43:40 +0200

    changing host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true 
   config.action_mailer.delivery_method = :smtp
-  host = 'tg-movie-rating.herokuapp.com' 
+  host = 'https://tg-movie-rating.herokuapp.com' 
   config.action_mailer.default_url_options = { host: host } 
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net', 
