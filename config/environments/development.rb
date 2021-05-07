4c2c99fd5afc34f570bd610852bd089766834c5e
tganz <tganz5890@googlemail.com>
2021-05-07 13:38:52 +0200

    fix email reset password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,10 +34,23 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.delivery_method = :smtp
+  host = 'http://localhost:3000'
+  config.action_mailer.default_url_options = { host: host } 
+  ActionMailer::Base.smtp_settings = {
+    :address => 'smtp.sendgrid.net', 
+    :port => '587',
+    :authentication => :plain,
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'], 
+    :domain => 'heroku.com', 
+    :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
