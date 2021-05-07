45b6b7f9e38d4fe311c3366c8ed6ca3f9983f6e1
tganz <tganz5890@googlemail.com>
2021-05-07 10:42:50 +0200

    setting up sendgrid for production email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.raise_delivery_errors = true config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true 
+  config.action_mailer.delivery_method = :smtp
   host = 'tg-movie-rating.herokuapp.com' 
   config.action_mailer.default_url_options = { host: host } 
   ActionMailer::Base.smtp_settings = {
